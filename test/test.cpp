ed803f12feab1f9d2e5459c3ff3ccc4af4d694fb
Dennis <bouncy.lu@hotmail.com>
2020-02-22 14:40:28 +0800

    Add Assignment 1 Testcase

--- test/test.cpp
+++ test/test.cpp
@@ -1,20 +1,35 @@
 #include <iostream>
 using namespace std;
 
-void swap(int *a, int *b)
-{
-    int *temp = a;
-    a = b;
-    b = temp;
+void g(int& k);
+
+int main(){
+    int y = 9;
+    int x = 7;
+    int i = 0;
+
+    while (i < 3){
+        if (i % 2 == 0){
+            g(x);
+            i++;
+        }
+        else{
+            g(y);
+            i++;
+        }
+    }
 }
 
-int main()
-{
-    int a[] = {1, 100};
-    int *p = a;
-    int *q = a + 1;
-    cout << *p << *q << endl;
-    swap(p, q);
-    cout << *q << *p << endl;
-    return 0;
+void g(int& k){
+    static int num = 6;
+
+    if (k % 2 == 0){
+        num += 2;
+        k = k + 6;
+    }
+    else{
+        num -= 2;
+        k = k - 6;
+    }
+    cout << num << ", " << k << endl;
 }
