05ab9d17378cdc573cd08c87480cbc189d0bb1d7
Dennis <bouncy.lu@hotmail.com>
2020-02-24 17:57:26 +0800

    4.cpp finished

--- Assignment 1/4.cpp
+++ Assignment 1/4.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int s;
-    char k;
+int main()
+{
+    int k = 0;
+    char s = '\000', c = '\000';
+    cin >> s >> k;
+    if (s == 'd')
+    {
+        k = -k;
+    }
+    while (true)
+    {
+        cin >> c;
+        if (c == '!')
+        {
+            cout << c;
+            break;
+        }
+        if (!(c >= 65 && c <= 90 || c >= 97 && c <= 122))
+        {
+            cout << c;
+            continue;
+        }
+        if (c >= 65 && c <= 90)
+        {
+            c = (((c - 65 + k) % 26) + 26) % 26 + 97;
+            cout << c;
+        }
+        else
+        {
+            c = (((c - 97 + k) % 26) + 26) % 26 + 65;
+            cout << c;
+        }
+    }
+    cout << endl;
     return 0;
 }
